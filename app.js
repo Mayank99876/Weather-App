cd77e6f37188d03a1e60c8f496031ac148451e47
Mayank99876 <109804941+Mayank99876@users.noreply.github.com>
2023-07-13 18:02:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+const staticPath = path.join(__dirname , "../Sample_Major_Project");
+app.use(express.static(staticPath));
+
+app.get("/", (req,res) => {
+    res.sendFile(__dirname + "/index.html");
+})
+
+
+app.post("/", (req,res) => {
+    const query = req.body.cityName;
+    const appId = "cd08b5f8da16d8f4d7b0e40bfdd86f7d";
+    const units = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + appId + "&units=" + units;
+    https.get(url,function(response){
+        console.log(response.statusCode);
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description; 
+            console.log(weatherDescription);
+           const icon = weatherData.weather[0].icon;
+           const imageURL = "http://openweathermap.org/img/wn/"+ icon +"@2x.png";
+           
+           const htmlResponse = `
+           <html>
+           <head>
+           <link rel="stylesheet" href="response.css">
+           </head>
+           <body>
+            <div class="container">
+             <div class="weather-container">
+                <h1>Weather Forecast</h1>
+                <div>
+                   <p>The weather is currently ${weatherDescription}</p>
+                   <img src="${imageURL}" alt="Example Image"></img>
+                   <h1>${temp} °C, ${query}</h1>
+                   <p>${new Date()}</p>
+                </div>
+
+             </div>
+
+           </div>
+           </body>
+           </html>
+           `;
+           res.send(htmlResponse);
+})
+ })
+})
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000.");
+})
