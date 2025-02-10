4bc0480b43cac2be9bc6b4ddf696b7ca6db310ce
Mayank Agarwal <109804941+Mayank99876@users.noreply.github.com>
2025-02-10 16:49:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,62 +4,57 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const staticPath = path.join(__dirname , "../Sample_Major_Project");
-app.use(express.static(staticPath));
+// Serve static files correctly
+app.use(express.static(__dirname));
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
-})
+});
 
-
-app.post("/", (req,res) => {
+app.post("/", (req, res) => {
     const query = req.body.cityName;
     const appId = "cd08b5f8da16d8f4d7b0e40bfdd86f7d";
     const units = "metric";
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + appId + "&units=" + units;
-    https.get(url,function(response){
-        console.log(response.statusCode);
-        response.on("data", function(data){
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${appId}&units=${units}`;
+
+    https.get(url, function (response) {
+        response.on("data", function (data) {
             const weatherData = JSON.parse(data);
+
+            // Correctly extract values
             const temp = weatherData.main.temp;
-            const weatherDescription = weatherData.weather[0].description; 
-            console.log(weatherDescription);
-           const icon = weatherData.weather[0].icon;
-           const imageURL = "http://openweathermap.org/img/wn/"+ icon +"@2x.png";
-           
-           const htmlResponse = `
-           <html>
-           <head>
-           <link rel="stylesheet" href="response.css">
-           </head>
-           <body>
-            <div class="container">
-             <div class="weather-container">
-                <h1>Weather Forecast</h1>
-                <div>
-                   <p>The weather is currently ${weatherDescription}</p>
-                   <img src="${imageURL}" alt="Example Image"></img>
-                   <h1>${temp} °C, ${query}</h1>
-                   <p>${new Date()}</p>
-                </div>
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
 
-             </div>
-
-           </div>
-           </body>
-           </html>
-           `;
-           res.send(htmlResponse);
-})
- })
-})
-
-
-
-
+            // Send the response
+            const htmlResponse = `
+                <html>
+                <head>
+                    <link rel="stylesheet" href="response.css">
+                </head>
+                <body>
+                    <div class="container">
+                        <div class="weather-container">
+                            <h1>Weather Forecast</h1>
+                            <div>
+                                <p>The weather is currently ${weatherDescription}</p>
+                                <img src="${imageURL}" alt="Weather Icon">
+                                <h1>${temp} °C, ${query}</h1>
+                                <p>${new Date()}</p>
+                            </div>
+                        </div>
+                    </div>
+                </body>
+                </html>
+            `;
+            res.send(htmlResponse);
+        });
+    });
+});
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000.");
-})
+});
